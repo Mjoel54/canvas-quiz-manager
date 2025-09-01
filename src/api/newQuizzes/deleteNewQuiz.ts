e21bc5a9adf24c2b6898043cf9467cbbc95eae89
Mitch <mitchjoelklein@hotmail.com>
2025-09-02 07:38:32 +1000

    deleteNewQuiz

---
+++ src/api/newQuizzes/deleteNewQuiz.ts
@@ -0,0 +1,42 @@
+import dotenv from "dotenv";
+import { NewQuiz } from "./types";
+
+dotenv.config();
+
+const baseUrl = process.env.BASE_URL;
+const apiToken = process.env.API_TOKEN;
+
+export async function deleteNewQuiz(
+  courseId: number,
+  assignmentId: number
+): Promise<NewQuiz> {
+  if (!baseUrl || !apiToken) {
+    throw new Error("Missing required variables");
+  }
+
+  const url = `${baseUrl}/api/quiz/v1/courses/${courseId}/quizzes/${assignmentId}`;
+
+  try {
+    const response = await fetch(url, {
+      method: "DELETE",
+      headers: {
+        Authorization: `Bearer ${apiToken}`,
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const deletedQuiz = (await response.json()) as NewQuiz;
+    console.log(deletedQuiz);
+    return deletedQuiz;
+  } catch (error) {
+    console.error("Error fetching quizzes:", error);
+    throw error;
+  }
+}
+
+// Example usage
+// deleteNewQuiz(945, 2280);

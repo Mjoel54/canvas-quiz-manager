22ac2c12c0f65e7fec82e0352797f47846d9dc6e
Mitch <mitchjoelklein@hotmail.com>
2025-09-01 21:01:01 +1000

    createNewQuiz

--- src/api/newQuizzes/types.ts
+++ src/api/newQuizzes/types.ts
@@ -1,24 +1,24 @@
 export interface NewQuiz {
-  id: string;
+  id?: string;
   title: string;
   instructions: string;
-  assignment_group_id: string;
+  assignment_group_id?: string;
   points_possible: number;
   due_at: string;
   lock_at: string | null;
   unlock_at: string | null;
   published: boolean;
   grading_type: string;
-  quiz_settings: QuizSettings | null;
+  quiz_settings?: QuizSettings | null;
 }
 
 export interface QuizSettings {
-  calculator_type: string;
+  calculator_type: "none" | "basic" | "scientific";
   filter_ip_address: boolean;
-  filters: {
+  filters?: {
     ips: string[][];
   };
-  one_at_a_time_type: string;
+  one_at_a_time_type: "none" | "question";
   allow_backtracking: boolean;
   shuffle_answers: boolean;
   shuffle_questions: boolean;
@@ -34,7 +34,7 @@ export interface MultipleAttemptsSettings {
   multiple_attempts_enabled: boolean;
   attempt_limit: boolean;
   max_attempts: number;
-  score_to_keep: string;
+  score_to_keep: "average" | "first" | "highest" | "latest";
   cooling_period: boolean;
   cooling_period_seconds: number;
 }
@@ -45,13 +45,21 @@ export interface ResultViewSettings {
   display_points_possible: boolean;
   display_items: boolean;
   display_item_response: boolean;
-  display_item_response_qualifier: string;
+  display_item_response_qualifier:
+    | "always"
+    | "once_per_attempt"
+    | "after_last_attempt"
+    | "once_after_last_attempt";
   show_item_responses_at: string;
   hide_item_responses_at: string;
   display_item_response_correctness: boolean;
-  display_item_response_correctness_qualifier: string;
+  display_item_response_correctness_qualifier: "always" | "after_last_attempt";
   show_item_response_correctness_at: string;
   hide_item_response_correctness_at: string;
   display_item_correct_answer: boolean;
   display_item_feedback: boolean;
+}
+
+export interface CreateNewQuizParams {
+  quiz: NewQuiz;
 }

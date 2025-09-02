2f1facedd37243d38f293c2dca834e851a3399f0
Mitch <mitchjoelklein@hotmail.com>
2025-09-02 19:02:38 +1000

    electron setup

---
+++ main.ts
@@ -0,0 +1,32 @@
+import { app, BrowserWindow } from "electron";
+import path from "path";
+
+function createWindow(): void {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  });
+
+  win.loadFile(path.join(__dirname, "..", "index.html"));
+
+  // Open the DevTools.
+  // win.webContents.openDevTools();
+}
+
+app.whenReady().then(createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});

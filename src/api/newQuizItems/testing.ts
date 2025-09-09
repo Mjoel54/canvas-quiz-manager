5cedb7a491f965c365e68d0fd779246e1d5a6ce9
Mitch <mitchjoelklein@hotmail.com>
2025-09-09 18:11:15 +1000

    minor code cleanup

--- src/api/newQuizItems/testing.ts
+++ src/api/newQuizItems/testing.ts
@@ -7,56 +7,9 @@ import {
   isValidTrueFalseRequestData,
 } from "./index";
 import data from "./data.json";
-import { run } from "node:test";
 
 const testCourseId = process.env.COURSE_ID;
 const assignmentId = process.env.NEW_QUIZ_ID;
-
-async function runCreateQuestions(data: { questions: any[] }) {
-  try {
-    const results: any = [];
-
-    for (const question of data.questions) {
-      const slug = question?.item?.entry?.interaction_type_slug;
-
-      switch (slug) {
-        case "choice":
-          if (!isValidMultipleChoiceRequestData(question)) {
-            throw new Error("❌ Invalid multiple choice question request");
-          }
-          break;
-
-        case "true-false":
-          if (!isValidTrueFalseRequestData(question)) {
-            throw new Error("❌ Invalid true/false question request");
-          }
-          break;
-
-        default:
-          throw new Error(
-            `❌ Unsupported or missing interaction_type_slug: ${slug}`
-          );
-      }
-
-      // Only runs if validation passed
-      const created = await createQuestionItemInNewQuiz(
-        Number(testCourseId),
-        Number(assignmentId),
-        question
-      );
-
-      results.push(created);
-      console.log(`✅ Created ${slug} item:`);
-    }
-
-    return results;
-  } catch (error) {
-    console.error("Batch creation failed:", error);
-    throw error;
-  }
-}
-
-runCreateQuestions(data);
 
 // runCreateTest();
 

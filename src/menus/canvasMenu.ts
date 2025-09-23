83bb6c1beeb2889d90fdafa18d76803691876613
Mitch <mitchjoelklein@hotmail.com>
2025-09-24 07:14:45 +1000

    edited Canvas menu

--- src/menus/canvasMenu.ts
+++ src/menus/canvasMenu.ts
@@ -1,22 +1,26 @@
 import inquirer from "inquirer";
-import { fetchAndDisplayNewQuizzes } from "./canvasMenus/fetchAndDisplayNewQuizzes.js";
-import { editNewQuizzes } from "./canvasMenus/editNewQuizzes.js";
+import { fetchAndDisplayNewQuizzes } from "./canvasMenuItems/fetchAndDisplayNewQuizzes.js";
+import { editNewQuizzes } from "./canvasMenuItems/editNewQuizzes.js";
 
 export async function showCanvasMenu() {
-  const { action } = await inquirer.prompt([
+  const answer = await inquirer.prompt([
     {
       type: "list",
       name: "action",
       message: "Canvas Actions:",
       choices: [
         { name: "Create a New Quiz", value: "create" },
-        { name: "List New Quizzes in a Course", value: "list" },
         { name: "Edit a New Quiz", value: "edit" },
+        { name: "List New Quizzes in a Course", value: "list" },
         { name: "Delete a New Quiz", value: "delete" },
         { name: "Back", value: "back" },
       ],
     },
   ]);
+
+  // console.log(`You selected: ${JSON.stringify(answer)}`);
+
+  const action = answer.action;
 
   if (action === "list") {
     await fetchAndDisplayNewQuizzes();

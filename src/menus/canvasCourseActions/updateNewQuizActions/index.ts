9ce921dd4a74287e895c71c0e59f21fc074cf1e9
Mitch <mitchjoelklein@hotmail.com>
2025-10-03 21:11:43 +1000

    handlePublishNewQuiz

--- src/menus/canvasCourseActions/updateNewQuizActions/index.ts
+++ src/menus/canvasCourseActions/updateNewQuizActions/index.ts
@@ -1,11 +1,9 @@
 import { handleListNewQuizItems } from "./handleListNewQuizItems.js";
 import { handleDeleteAllNewQuizItems } from "./handleDeleteAllNewQuizItems.js";
 import { handleAddNewQuizItems } from "./handleAddNewQuizItems.js";
-import { handlePublishNewQuiz } from "./handlePublishNewQuiz.js";
 
 export {
   handleListNewQuizItems,
   handleDeleteAllNewQuizItems,
   handleAddNewQuizItems,
-  handlePublishNewQuiz,
 };
